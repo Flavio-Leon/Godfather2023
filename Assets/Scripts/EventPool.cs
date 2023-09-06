b590093bd4d5b8fc798229becaf07432e368ae4d
Julien QUENTIN <jqntn88@gmail.com>
2023-09-06 21:00:27 +0200

    update eventpool

--- Assets/Scripts/EventPool.cs
+++ Assets/Scripts/EventPool.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace GF
 {
@@ -8,10 +10,23 @@ namespace GF
 
         [field: SerializeField] public float GameSpeed { get; private set; }
 
+        [SerializeField] private List<GameObject> Events = new();
+
         private void Awake() => Instance = this;
+
+        private void Start()
+        {
+            Time.timeScale = 0;
+        }
 
         public void Begin()
         {
+            Time.timeScale = 1;
+        }
+
+        public void RestartGame()
+        {
+            SceneManager.LoadScene(0);
         }
     }
 }
